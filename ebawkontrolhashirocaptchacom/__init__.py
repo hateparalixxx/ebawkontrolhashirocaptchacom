128910f560d92ae1d2b384ea68cf1b2760a23ae1
hateparalixxx <133792951+hateparalixxx@users.noreply.github.com>
2024-05-08 23:07:36 +0300

    Add files via upload

---
+++ ebawkontrolhashirocaptchacom/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from .ebawkontrolhashirocaptchacom import *
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import name as osn
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ebawkontrolhashirocaptchacom',
+    version='1.0.0',
+    author='Hashiro',
+    author_email='bababoyka999@google.com',
+    description='VKontakte captcha solver.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hateparalixxx/ebawkontrolhashirocaptchacom',
+    project_urls={
+        'Bug Tracker': 'https://github.com/hateparalixxx/ebawkontrolhashirocaptchacom/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    include_package_data = True,
+	install_requires = ['Pillow', 'numpy', 'requests', 'onnxruntime==1.17.3' if osn == 'nt' else 'onnxruntime>=1.9.0'],
+    python_requires='>=3.6'
+)
